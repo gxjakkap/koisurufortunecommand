6b689fc3a0bdcdc40c5af9bf093081ad27f7721f
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2025-10-06 23:26:39 +0700

    fix: refine sync workflow

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,7 +13,3 @@ curl -fsSL -o ~/.oph-tmp/demo.tar.gz https://objects.guntxjakka.me/demo.tar.gz
 tar -xzf ~/.oph-tmp/demo.tar.gz -C ~/oph2025
 
 rm -rf ~/.oph-tmp
-
-cp -Rn ~/oph2025/kfcmd-demo/* ~/oph2025/
-
-rm -rf ~/oph2025/kfcmd-demo
