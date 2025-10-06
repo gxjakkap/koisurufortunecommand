0e953177e4a8da95165582886e288395daba013a
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2025-10-06 17:08:58 +0700

    feat: add reset scripts

---
+++ scripts/reset.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ "$(basename "$PWD")" != "oph2025" ]; then
+  echo "You are not in oph2025"
+  exit 1
+fi
+
+mkdir -p ~/.oph-tmp
+
+curl -fsSL -o ~/.oph-tmp/demo.tar.gz https://objects.guntxjakka.me/demo.tar.gz
+
+rm -rf ./*
+
+tar -xzf ~/.oph-tmp/demo.tar.gz -C .
+
+rm -rf ~/.oph-tmp
+
+mv ./kfcmd-demo/** .
+
+rm -rf ./kfcmd-demo
---
+++ scripts/setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ "$PWD" = "$HOME" ]; then
+  cd "$HOME"
+fi
+
+mkdir -p ~/.oph-tmp
+mkdir -p ~/oph2025
+
+curl -fsSL -o ~/.oph-tmp/demo.tar.gz https://objects.guntxjakka.me/demo.tar.gz
+
+tar -xzf ~/.oph-tmp/demo.tar.gz -C ~/oph2025
+
+rm -rf ~/.oph-tmp
+
+mv ~/oph2025/kfcmd-demo/** .
+
+rm -rf ~/oph2025/kfcmd-demo
